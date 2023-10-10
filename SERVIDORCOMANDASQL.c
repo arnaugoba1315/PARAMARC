fcdcdac856fa3b3dba0c2b204a4e0ca140d9586a
arnaugoba1315 <arnau.gonzalez.barrios@estudiantat.upc.edu>
2023-10-10 19:40:44 +0200

    first commit

---
+++ SERVIDORCOMANDASQL.c
@@ -0,0 +1,141 @@
+//Incluir esta libreriￂﾭa para poder hacer las llamadas en shiva2.upc.es
+//include <my_global.h>
+#include <mysql.h>
+#include <string.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <unistd.h>
+
+int main(int argc, char **argv)
+{
+	MYSQL *conn;
+	int err;
+	// Estructura especial para almacenar resultados de consultas 
+	MYSQL_RES *resultado;
+	MYSQL_ROW row;
+	//Creamos una conexion al servidor MYSQL 
+	conn = mysql_init(NULL);
+	if (conn==NULL) {
+		printf ("Error al crear la conexi￳n: %u %s\n", 
+				mysql_errno(conn), mysql_error(conn));
+		exit (1);
+	}
+	//inicializar la conexin
+	conn = mysql_real_connect (conn, "localhost","root", "mysql", "db",0, NULL, 0);
+	if (conn==NULL) {
+		printf ("Error al inicializar la conexion: %u %s\n", 
+				mysql_errno(conn), mysql_error(conn));
+		exit (1);
+	}
+	char username[60];
+	printf ("Dame el username del jugador que jug� la partida 5\n"); 
+	scanf ("%s", username);
+	
+	char consulta [80];
+	strcpy (consulta,"SELECT Jugadores.Username FROM Jugadores,Partidas,Historial WHERE Partidas.id = 3 '");
+	strcat (consulta, username);
+	strcat (username,"'AND Partidas.id = Historial.idp AND Historial.idj= Jugadores.id and Historial.idj= Jugadores.id and Jugadores.id= 2	");
+	
+	err=mysql_query (conn, consulta);
+	if (err!=0) {
+		printf ("Error al consultar datos de la base %u %s\n",
+				mysql_errno(conn), mysql_error(conn));
+		exit (1);
+	}
+	//recogemos el resultado de la consulta. El resultado de la
+	//consulta se devuelve en una variable del tipo puntero a
+	//MYSQL_RES tal y como hemos declarado anteriormente.
+	//Se trata de una tabla virtual en memoria que es la copia
+	//de la tabla real en disco.
+	resultado = mysql_store_result (conn);
+	// El resultado es una estructura matricial en memoria
+	// en la que cada fila contiene los datos de una persona.
+	// Ahora obtenemos la primera fila que se almacena en una
+	// variable de tipo MYSQL_ROW
+	row = mysql_fetch_row (resultado);
+	
+	if (row == NULL)
+		printf ("No se han obtenido datos en la consulta\n");
+	else
+		while (row !=NULL) {
+			// la columna 0 contiene el nombre del jugador
+			printf ("%s\n", row[0]);
+			// obtenemos la siguiente fila
+			row = mysql_fetch_row (resultado);
+	}
+		
+		mysql_close (conn);
+		exit(0);
+		
+		int sock_conn, sock_listen, ret;
+	struct sockaddr_in serv_adr;
+	char peticion[512];
+	char respuesta[512];
+	// INICIALITZACIONS
+	// Obrim el socket
+	if ((sock_listen = socket(AF_INET, SOCK_STREAM, 0)) < 0)
+		printf("Error creant socket");
+	// Fem el bind al port
+	memset(&serv_adr, 0, sizeof(serv_adr));// inicialitza a zero serv_addr
+	serv_adr.sin_family = AF_INET;
+	// asocia el socket a cualquiera de las IP de la m?quina. 
+	//htonl formatea el numero que recibe al formato necesario
+	serv_adr.sin_addr.s_addr = htonl(INADDR_ANY);
+	// escucharemos en el port 9050
+	serv_adr.sin_port = htons(9050);
+	if (bind(sock_listen, (struct sockaddr *) &serv_adr, sizeof(serv_adr)) < 0)
+		printf ("Error al bind");
+	//La cola de peticiones pendientes no podr? ser superior a 4
+	if (listen(sock_listen, 4) < 0)
+		printf("Error en el Listen");
+	int i;
+	// Atenderemos solo 10 peticione
+	for(i=0;i<10;i++){
+		printf ("Escuchando\n");
+		
+		sock_conn = accept(sock_listen, NULL, NULL);
+		printf ("He recibido conexi?n\n");
+		//sock_conn es el socket que usaremos para este cliente
+		
+		// Ahora recibimos su peticion
+		ret=read(sock_conn,peticion, sizeof(peticion));
+		printf ("Recibida una petición\n");
+		// Tenemos que a?adirle la marca de fin de string 
+		// para que no escriba lo que hay despues en el buffer
+		peticion[ret]='\0';
+		
+		//Escribimos la peticion en la consola
+		
+		printf ("La petición es: %s\n",peticion);
+		char *p = strtok(peticion, "/");
+		int codigo =  atoi (p);
+		p = strtok( NULL, "/");
+		char nombre[20];
+		strcpy (nombre, p);
+		printf ("Codigo: %d, Nombre: %s\n", codigo, nombre);
+		
+		if (codigo ==1) //piden la longitd del nombre
+			sprintf (respuesta,"%d",strlen (nombre));
+		else
+			// quieren saber si el nombre es bonito
+			if((nombre[0]=='M') || (nombre[0]=='S'))
+			strcpy (respuesta,"SI");
+			else
+				strcpy (respuesta,"NO");
+			
+			// Enviamos la respuesta
+			write (sock_conn,respuesta, strlen(respuesta));
+			
+			// Se acabo el servicio para este cliente
+			close(sock_conn); 
+	}
+		
+		
+		
+		
+		
+		
+}
